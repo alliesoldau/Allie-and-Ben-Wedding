97a216562a1eb4286237fe51ea021eb8f4c9c90e
Allie Soldau <alliesoldau@gmail.com>
2022-12-15 20:57:24 -0500

    added more pics to details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -5,11 +5,18 @@ function Details() {
 
     return (
         <div>
-            <p>fuck even we don't know</p>
             <p>Venue: <a href="https://junefarms.com/">June Farms</a></p>
-            <div className="June-Farms-Logo">
-            </div>
-            <div className="Cows">
+            <div className="Details">
+                <div className="June-Farms-Logo">
+                </div>
+                <div className="Cows">
+                </div>
+                <div className="Pony-Barn">
+                </div>
+                <div className="Drinks">
+                </div>
+                <div className="Outdoor-Drinks">
+                </div>
             </div>
         </div>
     )
