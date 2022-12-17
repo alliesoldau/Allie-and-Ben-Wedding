0950ddf79faed3bb7a9bd36caa81ba59bb5edb39
Allie Soldau <alliesoldau@gmail.com>
2022-12-17 11:40:12 -0500

    basic venue info added

--- src/Components/DetailsVenue.js
+++ src/Components/DetailsVenue.js
@@ -3,10 +3,28 @@ import React from "react";
 function DetailsVenue() {
     return(
         <div className="Details-Venue">
-            {/* <p>Venue: <a href="https://junefarms.com/">June Farms</a></p> */}
             <div className="Venue-Details">
-                <div className="June-Farms-Logo">
+                {/* <div className="June-Farms-Logo">
                     <img /> 
+                </div> */}
+                <div className="Details-Venue-Text">
+                    <p>Our ceremony and reception will held at the beautiful <a href="https://junefarms.com/">June Farms</a>.</p>
+                    <p>June Farms sits on 120 acres in the New York Hudson 
+                        Valley and is home to highland cows, goats, horses, 
+                        ducks, chickens, pigs, and an adorable barn 
+                        cat named Roger.
+                    </p>
+                    <p>The ceremony will take place in a wooded forest area, 
+                        and the reception will take place between an outdoor 
+                        tent and a converted barn bar.
+                    </p>
+                    <p>Guests will be able to explore the farm and meet 
+                        the animals if they so desire!
+                    </p>
+                    <p>While the celebratory areas will be animal free, 
+                        those with allergies should be advised that 
+                        the farm animals are close by.
+                    </p>
                 </div>
             </div>
             <div className="Details-Venue-Images-Row1">
@@ -23,14 +41,6 @@ function DetailsVenue() {
                     <img /> 
                 </div>
             </div>
-            {/* <div className="Details-Venue-Images_Row2">
-                <div className="Tractor">
-                    <img /> 
-                </div>
-                <div className="Pony-Barn-Entrance">
-                    <img /> 
-                </div>
-            </div> */}
         </div>
     )
 }
